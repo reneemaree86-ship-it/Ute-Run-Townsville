7c65b1cac687260626b3cecd32276e7d21c3a9d7
reneemaree86-ship-it <reneemaree86@gmail.com>
2026-07-07 18:04:48 +1000

    Update next.config.js to clarify page extensions and exclude other directories

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Explicitly define page extensions to only process Next.js pages
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
 
 module.exports = nextConfig;
